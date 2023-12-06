6daea66e7633c0126b021dd56650371775aad67d
Matvey Karachun <karachun.m@mail.ru>
2023-12-06 21:33:45 +0300

    changed HtmlDialog visibility to public

--- src/main/java/factorymethod/html/HtmlDialog.java
+++ src/main/java/factorymethod/html/HtmlDialog.java
@@ -3,7 +3,7 @@ package factorymethod.html;
 import factorymethod.Button;
 import factorymethod.Dialog;
 
-class HtmlDialog extends Dialog {
+public class HtmlDialog extends Dialog {
     @Override
     public Button createButton() {
         return new HtmlButton();
